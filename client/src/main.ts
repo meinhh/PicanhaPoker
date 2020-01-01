de269155da277b955b3a712c3591f2c39dde8f55
Ido Shtivi <idoshtivi@gmail.com>
2020-01-02 01:41:19 +0200

    working on code editor ui

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,6 +5,13 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
+import VueCodemirror from 'vue-codemirror'
+Vue.use(VueCodemirror);
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/darcula.css';
+import 'codemirror/theme/monokai.css';
+import 'codemirror/mode/javascript/javascript.js'
+
 new Vue({
   router,
   store,
--- client/src/views/BotEditor.vue
+++ client/src/views/BotEditor.vue
@@ -1,30 +1,32 @@
 <template>
-    <div class="editor-page">
+    <div class="editor-page" :style="{'background-image': 'url(' + require('../assets/bg6.jpg') + ')'}">
         <h1>hello world</h1>
-        <codemirror v-model="code" :options="cmOptions"></codemirror>
+        <div class="editor-container">
+            <codemirror v-model="code" :options="cmOptions"></codemirror>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import PrismEditor from 'vue-prism-editor';
-import { codemirror } from 'vue-codemirror';
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import 'codemirror/lib/codemirror.css';
 
 @Component({
-  components: { codemirror },
 })
 export default class BotEditor extends Vue {
   @Prop() private msg!: string;
 
-  public code: string;
+  public code: string = 'const a = 10';
 
   public cmOptions = {
         tabSize: 4,
         mode: 'text/javascript',
-        theme: 'base16-dark',
+        theme: 'monokai',
         lineNumbers: true,
         line: true,
+        height: 100,
+        styleActiveLine: true,
+        matchBrackets: true,
+        showCursorWhenSelecting: true,
     }
 }
 </script>
@@ -32,10 +34,27 @@ export default class BotEditor extends Vue {
 <style lang="scss">
 .editor-page {
     height: 100%;
-    background: green;
+    // background: rgb(40,103,56);
+    // background-image: url('../assets/bg2.jpg')
+    background-size: 100% 100%;
 
     h1 {
         margin-top:0;
+    }
+
+    .CodeMirror {
+        height: 100%;
+    }
+
+    .editor-container {
+        opacity: 0.9;
+        height: 80%;
+        position: relative;
+
+        .vue-codemirror {
+            height: 100%;
+            text-align: left;
+        }
     }
 }
 </style>
