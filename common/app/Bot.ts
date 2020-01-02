b0da5b6003b1e12f50bfc7aabd08cdb620342902
Ido Shtivi <idoshtivi@gmail.com>
2020-01-03 00:38:57 +0200

    created models for user, bot, bot version

---
+++ common/app/Bot.ts
@@ -0,0 +1,8 @@
+export default class Bot {
+    public botId: number;
+    public name: string;
+    public dateCreated: Date;
+    public ownerUserId: number;
+    public activeVersionId?: number;
+    public avatarImageUrl?: string;
+}
